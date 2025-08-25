76b0259818666f92ea783da355fc013c9f654dfe
amlanpatnaik001 <amlanpatnaik001@gmail.com>
2025-08-25 20:44:45 +0530

    Add files via upload

---
+++ ASSIGNMENT-1.py
@@ -0,0 +1,14 @@
+
+N1 = int(input("Enter the first number: "))
+N2 = int(input("Enter the second number: "))
+
+addition = N1+N2
+subtraction = N1-N2
+multiplication = N1*N2
+division = N1/N2
+
+print("Addition: ", addition)
+print("Subtraction: ", subtraction)
+print("Multiplication: ", multiplication)
+print("Division: ", division)
+
---
+++ TASK-2.py
@@ -0,0 +1,6 @@
+FIRST_NAME=input("Enter First Name: ")
+LAST_NAME=input("Enter Last Name: ")
+
+full_name = FIRST_NAME + " " + LAST_NAME
+
+print("Hello, " + full_name + ", Welcome to the World!")
